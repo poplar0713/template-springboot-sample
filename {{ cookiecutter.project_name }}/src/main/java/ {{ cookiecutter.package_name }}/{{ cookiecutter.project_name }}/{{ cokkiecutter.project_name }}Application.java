fd9613b79ead649cfc25f4a853de4a671272f1cd
Jeongdong <duwjdehd0713@gmail.com>
2024-09-04 16:04:09 +0900

    init template

---
+++ {{ cookiecutter.project_name }}/src/main/java/ {{ cookiecutter.package_name }}/{{ cookiecutter.project_name }}/{{ cokkiecutter.project_name }}Application.java
@@ -0,0 +1,13 @@
+package {{ cookiecutter.package_name }}.{{ cookiecutter.project_name }};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class {{ cookiecutter.project_name }}Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run({{ cookiecutter.project_name }}Application.class, args);
+	}
+
+}
---
+++ {{ cookiecutter.project_name }}/src/test/java/{{ cookiecutter.package_name }}/{{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}ApplicationTests.java
@@ -0,0 +1,13 @@
+package {{ cookiecutter.package_name }}.{{ cookiecutter.project_name }};
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class {{ cookiecutter.project_name }}ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
